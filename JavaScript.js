24ebffdc5c3ef98de6a9c023ca23bcd33306c043
rahulroynipon <rahulroynipon@gmail.com>
2024-03-06 21:16:06 +0600

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,8 @@
+import { navbar } from "./component/navbar.js";
+
+
+const mainBody = document.querySelector("#root")
+
+
+
+navbar(mainBody)
---
+++ component/navbar.js
@@ -0,0 +1,25 @@
+function navbar(Parent){
+    const header = document.createElement("header");
+    header.setAttribute("class","navbar");
+    header.innerHTML = 
+    `
+    <nav class="h-screen">
+    <div class="flex items-center justify-center gap-4 mb-16 text-2xl"><img class="w-7 md:w-10" src="./icons/favicon.ico" alt=""><p class="icon_font">Friend Zone</p></div>
+
+
+    <ul class="flex flex-col ">
+        <li class="nav-item activePage"><img class="w-5 md:w-7" src="./icons/home.svg" alt=""><p>Home</p></li>
+        <li class="nav-item"><img class="w-5 md:w-7" src="./icons/people.svg" alt=""><p>People</p></li>
+        <li class="nav-item"><img class="w-5 md:w-7" src="./icons/gallery-add.svg" alt=""><p>Create</p></li>
+        <li class="nav-item"><img class="w-5 md:w-7" src="./icons/chat.svg" alt=""><p>Message</p></li>
+        <li class="nav-item"><img class="w-5 md:w-7" src="./icons/save.svg" alt=""><p>Saved</p></li>
+    </ul>
+
+    <div class="nav-item mt-40"><img class="w-5 md:w-7" src="./icons/logout.svg" alt=""><p>logout</p></div>
+</nav>
+    `
+    Parent.prepend(header);
+}
+
+
+export {navbar}
